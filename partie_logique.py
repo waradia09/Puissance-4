376956e9f8f14357acf189ea7e4fcaaa832915d0
Aboubacar Diawara <abdpro909@gmail.com>
2020-08-23 06:57:45 +0200

    third commit, human can play with computer

--- partie_logique.py
+++ partie_logique.py
@@ -1,5 +1,5 @@
 __AUTHOR__ = "Aboubacar DIAWARA"
-__EMAIL__ = "aboubacardiawara909@gmail.com"
+__EMAIL__ = "abdpro909@gmail.com"
 ___DATE___ = "18/08/2020"
 
 # Importation de modules
@@ -127,7 +127,7 @@ def show_grid(grid:list)->None:
 
 def is_valide_column(c:int, grid:list)->bool:
 	"""
-		Check if the the column c of the grid g can be shoosed by the player.
+		Check if the the column c of the grid g can be choosed by the player.
 		:param c: (int)
 		:param grid: (list) 
 		:return: (bool)
@@ -180,7 +180,7 @@ def line_fall_disc(c:int, g:list)->int:
         >>> line_fall_disc(3, g)
         0
     """
-	assert is_valide_column(c, g), "The colum must be valid"
+	#assert is_valide_column(c, g), "The colum must be valid"
 	index_line = -1
 	for line in g:
 		contain_column = line[c]
@@ -198,6 +198,14 @@ def modify_grid(g:list, p:str, c:int)->None:
         :param p: (str)	the player
         :param c: (int) the column
         :return: (None)
+
+        :Example:
+        >>> g = initialize_grid(5, 5)
+        >>> g1 = initialize_grid(5, 5)
+        >>> modify_grid(g, 2, 4)
+        >>> g1[4][4] = 2
+        >>> g1 == g
+        True
     """
 	line = line_fall_disc(c, g)
 	g[line][c] = p
@@ -235,7 +243,7 @@ def play(p1, p2, grid, is_ia_player1=False, is_ia_player2=False):
 
 		# if player is an IA, the computer will choose the column
 		if is_ia[player]:
-			c = is_alea(grid)
+			c = ia_win(grid, player)
 		else:
 			c = enter_column(player, grid)
 		r = line_fall_disc(c, grid)
@@ -251,9 +259,9 @@ def play(p1, p2, grid, is_ia_player1=False, is_ia_player2=False):
 
 # 2.5 Winner
 """
-	When the player p shoose the column c, the disc will be in the column c at the line r.
+	When the player p choose the column c, the disc will be in the column c at the line r.
 
-	To know if player p is the winner, we must sheck if the column he/she/it shoose has provoked an alignment of 4 disc.
+	To know if player p is the winner, we must sheck if the column he/she/it choose has provoked an alignment of 4 disc.
 	
 	The alignment can be on the line r, on the column c or one of the twos the diagonals who contain the case (r, c).
 
@@ -314,13 +322,36 @@ def is_align4(g:list, lc:list, p:int)->bool:
 		False
 		>>> is_align4(grid2, lc, 2)
 		True
+		>>> grid3 = [[0, 0, 0, 0, 0],
+                ...  [0, 0, 0, 1, 0],
+                ...  [0, 0, 1, 2, 0],
+                ...  [0, 1, 2, 1, 0],
+                ...  [1, 2, 2, 2, 0]]
+		>>> r, c = 1, 3
+		>>> lc = [(r-3, c+3), (r-2, c+2),
+		...  (r-1, c+1), (r, c), (r+1, c-1),
+		...  (r+2, c-2), (r+3, c-3)]
+		>>> is_align4(grid3, lc, 1)
+		True
+		>>> r, c = 2, 2
+		>>> grid4 = [[1, 0, 0, 0, 0],
+                ...  [2, 1, 0, 1, 0],
+                ...  [1, 2, 1, 2, 0],
+                ...  [2, 1, 2, 1, 0],
+                ...  [1, 2, 2, 1, 0]]
+                >>> lc = [(r-3, c-3), (r-2, c-2), (r-1, c-1), (r, c),(r + 1, c+1), (r+2, c+2), (r+3, c+3)]
+		>>> is_align4(grid4, lc, 1)
+		True
+		
 	"""		
 
 	valor_consecutives_cases = ""
 	for r, c in lc:
+		#print((r, c), is_valid_case((r, c), g))
 		if is_valid_case((r, c), g):
 			valor = g[r][c]
 			valor_consecutives_cases += str(valor)
+	#print("Fin: " + str(p)*4 + "vs" + valor_consecutives_cases)
 	return str(p)*4 in valor_consecutives_cases
 
 
@@ -346,8 +377,9 @@ def is_win(g:list, r:int, c:int, p:int)->bool:
 
 		:Exemple:
 		>>> g = [[1, 2, 2, 1, 1, 1, 1, 2]]
-		>>> is_win(g, 0, 6, 1)
+		>>> is_win(g, 0, 5, 1)
 		True
+		>>> 
 	"""
 
 	# list of horizontal cases
@@ -357,11 +389,11 @@ def is_win(g:list, r:int, c:int, p:int)->bool:
  	# lists of vertical cases
 	lc_diag1 = [(r-3, c-3), (r-2, c-2),
 			    (r-1, c-1), (r, c), 
-			    (r + 1, c+1), (r+2, c+2), (r+3, c+3)]
+			    (r+1, c+1), (r+2, c+2), (r+3, c+3)]
 
-	lc_diag2 = [(r-3, c+3), (r-2, c+2),
-			    (r-1, c+1), (r, c), (r+1, c-1),
-			    (r+2, c-2), (r+3, c-3)]
+	lc_diag2 = [(r+3, c-3),
+			    (r+2, c-2), (r+1, c-1), (r, c), (r-1, c+1), 
+			    (r-2, c+2), (r-3, c+3)]
 
     # list of vertical cases
 	lc_vertic = [(r-3, c), (r-2, c), (r-1, c),
@@ -369,10 +401,11 @@ def is_win(g:list, r:int, c:int, p:int)->bool:
 
 	return is_align4(g, lc_vertic, p) or is_align4(g, lc_diag1, p) or is_align4(g, lc_diag2, p) or is_align4(g, lc_horiz, p)
 
-# Implemantation of the IA
+### 3 Implemantation of the IA
+## 3.1: randomly shoot
 def is_alea(grid:list)->bool:
 	"""
-		return 
+		return randomly a valid column of th grid grid.
 		:param grid: (list) a grid
 
 		:Exemple:
@@ -387,24 +420,192 @@ def is_alea(grid:list)->bool:
 		is_valid_col = is_valide_column(c, grid)
 	return c
 
+## 3.2 Analyse all the possibilities
+# 3.2.1 Analyse of the victorie
+"""
+	To upgrade a little bit the decision of the computer, it'll try every column: it modify the grid by pllaying the column c, then it determin (with the function is_win) if it's a winning shoot.
+	If that is the case, it'll choose the current column, otherwise it'll will check the next column.
+
+	Let implemant the function unmove who allows to cancel a computer choose then the function ia_win(g, p)
+"""
+
 def unmove(grid:list, c:int)->None:
     """
-    	modify the grid by reoving the last disc who has been put in the column c.
+    	modify the grid by removing the last disc who has been put in the column c.
         :param grid: (list) the grid
         :param c: (int) the column
         :return : (None) moify the grid
+
+        :Example:
+        >>> g = initialize_grid(5, 5)
+        >>> g1 = initialize_grid(5, 5)
+        >>> modify_grid(g, 2, 4)
+        >>> g1[4][4] = 2
+        >>> g1 == g
+        True
+        >>> unmove(g, 4)
+        >>> g1[4][4] = 0
+        >>> g1 == g
+        True
     """
     r = line_fall_disc(c, grid)
-    grid[r-1][c] = 0
+    grid[r+1][c] = 0
         
-        
+def ia_win(g:list, p:int)->int:
+	"""
+		return the column who can help the computer to win the game, if that is not possible, it's return randomly one column.
+
+		:param g: (list) a grid
+		:param p: (int) a player
+		:return : (int) a column
+		:CU: None
+
+		:Exemple:
+		>>> g = [[1, 2, 2, 1, 1, 0, 1, 2]]
+		>>> ia_win(g, 1)
+		5
+		>>> g =     [[1, 0, 0, 0, 0],
+                ...  [2, 1, 0, 0, 0],
+                ...  [1, 2, 1, 2, 0],
+                ...  [2, 1, 2, 2, 0],
+                ...  [1, 2, 2, 1, 2]]
+        >>> ia_win(g, 1)
+        3
+
+
+	"""
+	for c in range(nc(g)):
+		if is_valide_column(c, g):
+			r = line_fall_disc(c, g)
+			play_one_step(p, g, c)
+
+			#print(c, g, is_win(g, r, c, p))
+			
+			if is_win(g, r, c, p):
+				unmove(g, c)
+				return c
+			else:
+				unmove(g, c)
+
+			#print(c, g, '\n')
+	return is_alea(g)
+
+
+# 3.2.2 Evaluation of the grid.
+"""
+	We're still improving the choose of the computer a little bit. This time for each column tried, the IA will analyse the entire grid (not just the victory around the square played).
+
+	The analysis of the entire grid will result in a score. A hight score will correspond to a 'favorable grid' for an IA unlike a small score. So the IA will try therefore test each of the possible columns (as previously) and choose the one that leads to the most favorable grid (the one with the highest score).
+
+	Now, let determinate the the function for the evaluation of the score. A possible strategie is to test all the quadruplets of the possible consecutives cases of the grid. 
+"""
+
+def score_quadruplet(grid, quadruplet:list, p)->int:
+	"""
+		return the score of the qudruplet
+		:param grid: 
+		:param quadruplet: (list) list of posiiton (r_i, c_i)
+		:param p: (int) the IA
+		:return : (int) the score of the quadruplet
+		:CU:
+
+		:Exemple:
+		>>> g =     [[1, 0, 0, 0, 0],
+                ...  [2, 1, 0, 0, 0],
+                ...  [1, 2, 1, 2, 0],
+                ...  [2, 1, 2, 2, 0],
+                ...  [1, 2, 2, 1, 2]]
+		>>> score_quadruplet(g, [(1, 2), (2, 2), (3, 2), (4, 2)], 1)
+		0
+		>>> score_quadruplet(g, [(4, 0), (3, 1), (2, 2), (1, 3)], 1)
+		1000
+	"""
+	assert len(quadruplet) == 4, f"Attention, {quadruplet} n'est pas un quadruplet"
+	list_pawns = [grid[r][c] for r, c in quadruplet]
+	if list_pawns.count(1) > 0 and list_pawns.count(2) > 0 or list_pawns.count(0) == 4:
+		return 0
+	elif list_pawns.count(p) + list_pawns.count(0) == 4:
+		if list_pawns.count(p) == 1:
+			return 1
+		elif list_pawns.count(p) == 2:
+			return 10
+		elif list_pawns.count(p) == 3:
+			return 1000
+		elif list_pawns.count(p) == 4:
+			return 100000
+	elif list_pawns.count(p) == 0 and list_pawns.count(0) < 4:
+		if list_pawns.count(0) == 3:
+			return -1
+		elif list_pawns.count(0) == 2:
+			return -10
+		elif list_pawns.count(0) == 1:
+			return -500
+
+def evaluate_score_case(grid:list, case:tuple, p)->int:
+	"""
+		return the score of case in the grid.
+		:param grid: (list) a grid.
+		:param case: (tuple) position in the grid.
+		:return: (int) the score of case.
+		:param p: (int) the IA.
+		:CU: Nothing
+
+		:Exemple:
+		>>> g =     [[1, 0, 0, 0, 0],
+                ...  [2, 1, 0, 0, 0],
+                ...  [1, 2, 1, 2, 0],
+                ...  [2, 1, 2, 2, 0],
+                ...  [1, 2, 2, 1, 2]]
+        >>> evaluate_score_case(g, (0, 3), 2)
+        20
+	"""
+	r, c = case
+	number_lines = nr(grid)
+	number_columns = nc(grid)
+	score = 0
+	if r + 3 <= number_lines: # vertical bottom quadruplet
+		quadruplet = [(r, c), (r+1, c), (r+2, c), (r+3, c)]
+		score += score_quadruplet(grid, quadruplet, p)
+	if c + 3 <= number_columns: # horizontal right quadruplet
+		quadruplet = [(r, c), (r, c+1), (r, c+2), (r, c+3)]
+		score += score_quadruplet(grid, quadruplet, p)
+	if c  >= 3 and r + 3 <= number_lines: # diagonal left quadruplet
+		quadruplet = [(r, c), (r+1, c-1), (r+2, c-2), (r+3, c-3)]
+		score += score_quadruplet(grid, quadruplet, p)
+	if r + 3 <= number_lines and c + 3 <= number_columns: # diagonal right quadruplet
+		quadruplet = [(r, c), (r+1, c+1), (r+2, c+2), (r+3, c+3)]
+		score += score_quadruplet(grid, quadruplet, p)
+	return score
+	
+def score_grille(g:list, p:int)->int:
+	"""
+		return the sum of the scores of the entire grid
+	"""
+	return sum(evaluate_score_case(g, (r, c), p) for r in g for c in r)
+
+
+
+
 
 if __name__ == '__main__':
 	import doctest, time
 	doctest.testmod()
 
 	grid = initialize_grid(10, 10)
-	play(1, 2, grid, is_ia_player1=True)
+	#play(1, 2, grid, is_ia_player1=True)
+	###### Debugage
+	"""
+	grid3 = [[0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0],
+                [0, 0, 1, 2, 0],
+                [0, 1, 2, 1, 0],
+                [1, 2, 2, 2, 0]]
+	r, c = 2, 2
+	lc = [(r-3, c-3), (r-2, c-2), (r-1, c-1), (r, c),(r + 1, c+1), (r+2, c+2), (r+3, c+3)]
+	is_align4(grid3, lc, 2)
+	"""
+	###### Fin debugage
+	
 
 	wait_quit()
 	time.sleep(5)
